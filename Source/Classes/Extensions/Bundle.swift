ee8cea2274818b66674e2104400895d761b02873
Son Nguyen <sonnn1992@gmail.com>
2018-12-31 04:23:57 +0700

    no message

--- Source/Classes/Extensions/Bundle.swift
+++ Source/Classes/Extensions/Bundle.swift
@@ -10,7 +10,7 @@ import Foundation
 extension Bundle {
     
     class var podBundle: Bundle {
-        let podBundle = Bundle(for: EmojiView.classForCoder())
+        let podBundle = Bundle(for: EmojiView.self)
         
         guard let bundleURL = podBundle.url(forResource: "ISEmojiView", withExtension: "bundle") else {
             fatalError("Could not create a path to the bundle")
